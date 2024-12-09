c168c2781bf1ffec521a0d4323b8907eec178f06
Mahindha-04 <mc@elitetechpark.com>
2024-12-09 11:06:12 +0530

    suma

--- client/main.js
+++ client/main.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 	// All URLs to the Advanced I/O function will be of the pattern: /server/{function_name}/{url_path}
     $.ajax({
         type: 'GET',
-          url: '/server/to_do_list_function/todo', //Ensure that 'to_do_list_function' is the package name of your function
+          url: '/server/tutorial_authentication_master_function/todo', //Ensure that 'to_do_list_function' is the package name of your function
           async: false,
         success: function(html){
             //Appends the items to the HTML from the server on success
@@ -21,7 +21,7 @@ $(document).ready(function(){
         $.ajax({
             type: 'POST',
             contentType: "application/json; charset=utf-8",
-            url: '/server/to_do_list_function/todo', //Ensure that 'to_do_list_function' is the package name of your function
+            url: '/server/tutorial_authentication_master_function/todo', //Ensure that 'to_do_list_function' is the package name of your function
             data: JSON.stringify(todo),
             success: function(data){
                 location.reload(); //Reloads the page on success
@@ -37,7 +37,7 @@ $(document).ready(function(){
         var itemRowId = $(this).attr("value"); //The item is deleted using the attribute "value" which indicates the ROWID of the record in the Data Store
 		$.ajax({
 		  type: 'DELETE',
-		  url: '/server/to_do_list_function/todo' + itemRowId, //Appends the ROWID along with the URL 
+		  url: '/server/tutorial_authentication_master_function/todo' + itemRowId, //Appends the ROWID along with the URL 
 		//Ensure that 'to_do_list_function' is the package name of your function
 		  success: function(data){
               location.reload(); //Reloads the page on success
